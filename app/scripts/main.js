5fd91be000da016e189250c6ca12e9c5139d2b15
Diego <diego@upperquad.com>
2015-01-29 04:39:58 -0800

    Update FA

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -191,5 +191,14 @@
         }
       });
     }
+
+    function toCamelCase(sentenceCase) {
+      var out = '';
+      sentenceCase.split(' ').forEach(function (el, idx) {
+        var add = el.toLowerCase();
+        out += (idx === 0 ? add : add[0].toUpperCase() + add.slice(1));
+      });
+      return out;
+    }
   }
 }());
